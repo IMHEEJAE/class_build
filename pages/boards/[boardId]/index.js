ac45c930e685bace7b6c5861a1ca956668bd5d3d
IMHEEJAE <imheejae95@gmail.com>
2023-07-31 17:02:17 +0900

    '분기배포'

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -9,7 +9,7 @@ export default function BoardsPage() {
     </>
   );
 }
-export const getSeverSidePorps = () => {
-  // 만약 서버사이드 렌더링을 하는 페이지라면 ?? => out폴더로 생성 불가
-  //                                          => 이런 경우 , next.config.js 에서 exportPathMap으로 현재페이지 제외시키기
-}
+// export const getSeverSidePorps = () => {
+//   // 만약 서버사이드 렌더링을 하는 페이지라면 ?? => out폴더로 생성 불가
+//   //                                          => 이런 경우 , next.config.js 에서 exportPathMap으로 현재페이지 제외시키기
+// }
