7d6ff5ca243a2788d8311a0cedd391266f48cbaa
IMHEEJAE <imheejae95@gmail.com>
2023-07-31 16:54:50 +0900

    '분기배포'

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "heejae-deploy-project",
+
+  // 아래 주소들만 out폴더에 만들어줘! =>getSeverSideProps 있는 페이지는 제외
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
